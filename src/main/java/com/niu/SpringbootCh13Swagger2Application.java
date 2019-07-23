6925e303082300106cf36b780a3cf0292254b1dc
18300603955 <oceaniu@163.com>
2019-07-23 14:19:09 +0800

    deleteUserById

--- src/main/java/com/niu/SpringbootCh13Swagger2Application.java
+++ src/main/java/com/niu/SpringbootCh13Swagger2Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootCh13Swagger2Application {
 
+	/**
+	 * 访问 http://localhost:8080/swagger-ui.html 查看接口文档
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCh13Swagger2Application.class, args);
 	}
