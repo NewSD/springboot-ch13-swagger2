c072890f7e7d02ad0c75be1c03437e36d1abc947
18300603955 <oceaniu@163.com>
2019-07-23 13:48:13 +0800

    init

---
+++ src/main/java/com/niu/SpringbootCh13Swagger2Application.java
@@ -0,0 +1,13 @@
+package com.niu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCh13Swagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCh13Swagger2Application.class, args);
+	}
+
+}
