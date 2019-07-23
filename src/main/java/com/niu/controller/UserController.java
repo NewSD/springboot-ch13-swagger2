674b375f8e04965b6a1d413ce1b37948946d144e
18300603955 <oceaniu@163.com>
2019-07-23 14:27:36 +0800

    adduser

--- src/main/java/com/niu/controller/UserController.java
+++ src/main/java/com/niu/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.niu.controller;
 
+import com.niu.model.User;
 import io.swagger.annotations.*;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by ami on 2019/7/23.
@@ -16,7 +14,7 @@ public class UserController {
 
     @ApiOperation(value = "查询用户", notes = "根据id查询用户")
     @ApiImplicitParam(
-        paramType = "path",name = "id", value = "用户id",required = true
+            paramType = "path", name = "id", value = "用户id", required = true
     )
     @GetMapping("/user/{id}")
     public String getUserById(@PathVariable Integer id) {
@@ -25,12 +23,31 @@ public class UserController {
 
 
     @ApiResponses({
-            @ApiResponse(code = 200,message = "删除成功"),
-            @ApiResponse(code = 500,message = "删除失败")
+            @ApiResponse(code = 200, message = "删除成功"),
+            @ApiResponse(code = 500, message = "删除失败")
     })
-    @ApiOperation(value = "删除用户",notes = "根据ID删除用户")
+    @ApiOperation(value = "删除用户", notes = "根据ID删除用户")
     @DeleteMapping("/user/{id}")
-    public Integer deleteUserById(@PathVariable Integer id){
+    public Integer deleteUserById(@PathVariable Integer id) {
         return id;
     }
+
+    @ApiOperation(value = "添加用户", notes = "添加一个用户,传入用户名和地址")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "username", value = "用户名", required = true, defaultValue = "sang"),
+            @ApiImplicitParam(paramType = "query", name = "address", value = "用户地址", required = true, defaultValue = "shenzhen"),
+    })
+    @PostMapping("/user")
+    public String addUser(@RequestParam String username,
+                          @RequestParam String address) {
+        return username + ":" + address;
+    }
+
+    @ApiOperation(value = "修改用户", notes = "修改用户,传入用户信息")
+    @PutMapping("/user")
+    public String updateUser(@RequestBody User user) {
+        return user.toString();
+    }
+
+
 }
